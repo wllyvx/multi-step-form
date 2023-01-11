87ae45a9ac3ba2619b1ae92d0685831786289465
Willy Fajar Ramadhan <willyfr88@gmail.com>
2023-01-11 14:41:19 +0700

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,15 @@
 import './globals.css'
+import { Ubuntu } from '@next/font/google'
+import Link from 'next/link'
+
+const ubuntu = Ubuntu({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+})
+
+const steps = (title:number, link:string)=>{
+  return <li><Link href={link}>{title}</Link></li>
+}
 
 export default function RootLayout({
   children,
@@ -12,7 +23,17 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className={ubuntu.className}>
+        <main>
+          <ul className='flex justify-between'>
+            {steps(1, "/")}
+            {steps(2, "/")}
+            {steps(3, "/")}
+            {steps(4, "/")}
+          </ul>
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
