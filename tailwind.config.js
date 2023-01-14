1fef59f169051f6b156f50abb40e6e0902becd72
Willy Fajar Ramadhan <willyfr88@gmail.com>
2023-01-14 16:58:52 +0700

    add mobile background to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'sidebar-mobile' : "url('../public/images/bg-sidebar-mobile.svg')",
+      }
+    },
   },
   plugins: [],
 }
